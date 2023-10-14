18fe6d57482fd6666eb62375836d4989dd297472
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-15 00:45:50 +0300

    test yt loader

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,8 +1,13 @@
+import { YoutubeLoader } from "langchain/document_loaders/web/youtube";
 import { NextResponse } from "next/server";
 
 const handler = (req: Request) => {
     console.log("working....")
-    
+    const loader = YoutubeLoader.createFromUrl("", {
+        language: "en",
+        addVideoInfo: true,
+      });
+  
     return NextResponse.json({greetings: "hi"})
 }
   
